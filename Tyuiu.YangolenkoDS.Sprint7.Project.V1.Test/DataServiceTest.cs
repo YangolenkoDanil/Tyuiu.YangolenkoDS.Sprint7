9f79d54a5f02907915b8ebf4cb60a2568e65c6df
Tyuiu.YangolenkoDS@outlook.com <Tyuiu.YangolenkoDS@outlook.com>
2023-12-13 16:36:29 +0500

    Add project files.

---
+++ Tyuiu.YangolenkoDS.Sprint7.Project.V1.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.YangolenkoDS.Sprint7.Project.V1.Lib;
+
+namespace Tyuiu.YangolenkoDS.Sprint7.Project.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
